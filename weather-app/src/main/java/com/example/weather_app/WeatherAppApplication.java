41ab4220075db56c63410f9cb051088458389d22
“Sumire” <Uwaa0927@gmail.com>
2026-01-08 16:52:16 +0900

    feat: Hello Worldページを作成

---
+++ weather-app/src/main/java/com/example/weather_app/WeatherAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.weather_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppApplication {
+
+	public static void main(String[] args) {
+		//Spring Bootアプリを起動する
+		SpringApplication.run(WeatherAppApplication.class, args);
+	}
+
+}
---
+++ weather-app/src/main/java/com/example/weather_app/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.weather_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(Model model) {
+        //現在の日時を取得して(yyyy...)の形式に変換している
+        String currentTime = LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+
+        //コントローラー（HelloController.java）からデータをHTMLに渡す準備
+        model.addAttribute("currentTime", currentTime);
+
+        //hello.htmlを表示する
+        return "hello";
+    }
+}
